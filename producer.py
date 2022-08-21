53815195fe0fe0d8ca4fbdafd77778d3233cd8ac
Kokou <valentinkokou@gmail.com>
2022-08-21 21:21:39 +0200

    Added Keyboard input and mesage variable
    Added a new var 'message' and also keyboard input, if no input the previous "Hello World!" will be used

--- producer.py
+++ producer.py
@@ -1,12 +1,15 @@
 import pika
+import sys
+
+
 host = '192.168.0.146'
 port = 5672
 credentials = pika.PlainCredentials('guest','guest')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host, port,'/'))
 channel = connection.channel()
-
+message = ' '.join(sys.argv[1:]) or "Hello World!"
 channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='',routing_key='hello',body='Hello World!')
+channel.basic_publish(exchange='',routing_key='hello',body=message)
 
-print(" [x] Sent 'Hello World!")
+print(" [x] Sent %r" %message)
 connection.close()
