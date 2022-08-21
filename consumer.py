2f4a50996279dfac327d670f5ddb0bf0e278e9a4
Kokou <valentinkokou@gmail.com>
2022-08-21 21:19:58 +0200

    extended the consumer with time.sleep for simulation
    Extended file with time import and added to callback func the sleep func to wait 1 sec for every '.' in the received message

--- consumer.py
+++ consumer.py
@@ -1,4 +1,5 @@
-import pika, sys, os
+import pika, sys, os, time
+
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.146'))
@@ -7,7 +8,10 @@ def main():
     channel.queue_declare(queue='hello')
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        print(" [x] Received %r" % body.decode())
+
+        time.sleep(body.count(b'.'))
+        print(" [x] Done")
 
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
